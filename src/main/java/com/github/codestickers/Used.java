84a5ced83454cdb6b7c5b0889e9a948297555e31
conni <c.buschka@arcusx.com>
2018-05-03 21:01:25 +0200

    Added Used.

--- src/main/java/com/github/codestickers/Used.java
+++ src/main/java/com/github/codestickers/Used.java
@@ -1,4 +1,13 @@
 package com.github.codestickers;
 
+import java.lang.annotation.*;
+
+/**
+ * This annotations marks a section as used with an optional comment.
+ */
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Documented
 public @interface Used {
+    String value() default "";
 }
