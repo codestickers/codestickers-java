57aacdc1e186deb813ec588b62e3486b414e18d7
cbuschka <cbuschka@gmail.com>
2018-05-30 16:31:13 +0200

    Issue #2: Added @See.

---
+++ src/main/java/com/github/codestickers/See.java
@@ -0,0 +1,26 @@
+package com.github.codestickers;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation links to related information.
+ */
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.TYPE,
+        ElementType.FIELD,
+        ElementType.METHOD,
+        ElementType.PARAMETER,
+        ElementType.CONSTRUCTOR,
+        ElementType.LOCAL_VARIABLE,
+        ElementType.ANNOTATION_TYPE,
+        ElementType.PACKAGE,
+        ElementType.TYPE_PARAMETER,
+        ElementType.TYPE_USE})
+@Documented
+public @interface See {
+    String value() default "";
+}
