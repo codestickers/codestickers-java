6b0c40e06ea7a5823546b0c14b87ab9d3f7c2c5f
conni <c.buschka@arcusx.com>
2018-06-28 18:45:57 +0200

    Added @Note.

---
+++ src/main/java/com/github/codestickers/Note.java
@@ -0,0 +1,27 @@
+package com.github.codestickers;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Note to an element.
+ */
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.TYPE,
+        ElementType.FIELD,
+        ElementType.METHOD,
+        ElementType.PARAMETER,
+        ElementType.CONSTRUCTOR,
+        ElementType.LOCAL_VARIABLE,
+        ElementType.ANNOTATION_TYPE,
+        ElementType.PACKAGE,
+        ElementType.TYPE_PARAMETER,
+        ElementType.TYPE_USE})
+@Documented
+public @interface Note
+{
+    String value() default "";
+}
