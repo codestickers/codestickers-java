dd419bd958e325ee210bf3d94af15beed311ab9b
conni <c.buschka@arcusx.com>
2018-06-28 18:46:10 +0200

    Added @Unused.

---
+++ src/main/java/com/github/codestickers/Unused.java
@@ -0,0 +1,27 @@
+package com.github.codestickers;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotations marks a section as unused with an optional comment.
+ */
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.TYPE,
+        ElementType.FIELD,
+        ElementType.METHOD,
+        ElementType.PARAMETER,
+        ElementType.CONSTRUCTOR,
+        ElementType.LOCAL_VARIABLE,
+        ElementType.ANNOTATION_TYPE,
+        ElementType.PACKAGE,
+        ElementType.TYPE_PARAMETER,
+        ElementType.TYPE_USE})
+@Documented
+public @interface Unused
+{
+    String value() default "";
+}
